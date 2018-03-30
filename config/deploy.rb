5636a08fa758c6fbadcf553a560c24715aa47b6d
Matthew Zagaja <mzagaja@alum.wpi.edu>
2018-03-30 16:48:49 -0400

    Revert "Try suggested yarn fix"
    This reverts commit c150296cc97c1c52ff5431a1840c45284d62b21c.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,16 +37,3 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-before "deploy:assets:precompile", "deploy:yarn_install"
-
-namespace :deploy do
-  desc 'Run rake yarn:install'
-  task :yarn_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install")
-      end
-    end
-  end
-end

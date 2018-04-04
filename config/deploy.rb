b7168887cfc13ad425d473a9757818265be8d652
Ian Hill <ihill@mapc.org>
2018-04-04 15:33:18 -0400

    Revert name of install task to yarn_install

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-before "deploy:assets:precompile", "deploy:npm_install"
+before "deploy:assets:precompile", "deploy:yarn_install"

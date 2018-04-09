bec772d5c11aa11c4d1883c98f87ec482f71bc69
Ian Hill <ihill@mapc.org>
2018-04-09 09:09:41 -0400

    Rename and adjust booleans in schema and resources in routes:

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  scope '/api' do
-    scope '/city' do
-      get '/:name' => 'city#show'
-    end
-    scope '/road' do
-      get '/' => 'road#index'
-      get '/:id' => 'road#show'
-    end
-    scope '/node' do
-      get '/' => 'node#index'
-      get '/:id' => 'node#show'
-    end
-    scope '/plan' do
-      get '/' => 'plan#index'
-      post '/' => 'plan#create'
-      get '/:id' => 'plan#show'
-    end
+  namespace :api do
+    resources :city, only: [:show]
+    resources :road, only: [:index, :show]
+    resources :node, only: [:index, :show]
+    resources :plan, only: [:index, :create, :show]
   end
   # Let React Router take care of everything not API related
   get '*path', to: 'application#index'

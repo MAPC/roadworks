b8490915355f71004aefbf7c7d2a2f2b1e0c4225
Eric Youngberg <eyoungberg@mapc.org>
2018-04-17 11:19:01 -0400

    Add user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:utility, :city]
+
+
+  private
+
+  def set_default_role
+    self.role ||= :utility
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api do
+    devise_for :users
+
     resources :cities, only: [:show]
     resources :roads, only: [:index, :show]
     resources :nodes, only: [:index]
     resources :plans, only: [:index, :create, :show]
     resources :permits, only: [:index, :show]
   end
+
   # Let React Router take care of everything not API related
   get '*path', to: 'application#index'
 end
